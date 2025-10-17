cdf65710ef12a35a988e87d14e3badafd5e253fc
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-17 16:32:07 +0530

    code

--- CODEX_Project/src/App.jsx
+++ CODEX_Project/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/HOME/navbar/navbar.jsx'
+import Front from './Components/HOME/firstpage/front.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Front />
       <h2>Kanhaiya kumar</h2>
     </>
   )
---
+++ CODEX_Project/src/Components/HOME/firstpage/front.jsx
@@ -0,0 +1,31 @@
+import React  from "react";
+import './front.css';
+import academicImage from '../../../assets/academicImage.png';
+import codingImage from '../../../assets/codingImage.png';
+import happyStudents from '../../../assets/happyStudents.png';
+
+const Front = () => {
+    return (
+        <>
+        <div className="parentFront">
+            <div className="tittle">
+                <h1>
+                    <strong>Master Your Degree. Master Your Future :</strong>
+                </h1>
+                <h3><p>
+                    "Codex is the essential platform for the modern student, bridging the gap between academic success and technical literacy.
+                     We provide comprehensive university notes and sample papers alongside curated, 
+                     guided pathways—complete with video lectures—to launch disrupted students into the world of coding."
+                    </p>
+                    </h3>
+
+            </div>
+            <div className="imageSection">
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Front;
