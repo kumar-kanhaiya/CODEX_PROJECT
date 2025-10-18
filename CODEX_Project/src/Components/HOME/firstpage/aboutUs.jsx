4bf1e328a2d5b02274289038e9bb8fb628d3e409
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-18 09:22:39 +0530

    code

--- CODEX_Project/src/Components/HOME/firstpage/aboutUs.jsx
+++ CODEX_Project/src/Components/HOME/firstpage/aboutUs.jsx
@@ -19,8 +19,36 @@ const AboutUs = () => {
                     <h3>Comprehensive Notes</h3>
                     <p>Access detailed notes for better understanding</p>
                     </div>
-                    <div className="main"></div>
-
+                    <div className="main">
+                        <img src="https://img.icons8.com/?size=100&id=46520&format=png&color=000000" alt="" />
+                        <h3>Exam-Ready PYQ Kits</h3>
+                        <p>Prepare effectively with past year question kits</p>
+                    </div>
+                    <div className="main">
+                        <img src="https://img.icons8.com/?size=100&id=12486&format=png&color=000000" alt="" />
+                        <h3>Code Language Selector</h3>
+                        <p>Effortlessly pick your first language based on your goals.</p>
+                    </div>
+                    <div className="main">
+                        <img src="https://img.icons8.com/?size=100&id=7522&format=png&color=000000" alt="" />
+                        <h3>Distraction-Proof Lectures</h3>
+                        <p>Stay focused with our engaging lecture formats</p>
+                    </div>
+                    <div className="main">
+                        <img src="https://img.icons8.com/?size=100&id=K8zgVgFynQBo&format=png&color=000000" alt="" />
+                        <h3>Personalized Learning Paths</h3>
+                        <p>Get recommendations tailored to your learning style.</p>
+                    </div>
+                    <div className="main">
+                        <img src="https://img.icons8.com/?size=100&id=CQLOwoo3vusM&format=png&color=000000" alt="" />
+                        <h3>Post-Video Challenge Sets</h3>
+                        <p>Test your skills with coding problems after every video.</p>
+                    </div>
+                    <div className="main">
+                        <img src="https://img.icons8.com/?size=100&id=124182&format=png&color=000000" alt="" />
+                        <h3>Integrated Dual-Focus</h3>
+                        <p>Seamlessly balance academic study with your coding skill development.</p>
+                    </div>
                 </div>
             </div>
         </div>
