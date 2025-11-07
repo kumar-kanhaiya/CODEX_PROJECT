2a995293a84b8f24e56a66c4813bf73fbe3b5ffc
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-11-07 11:28:35 +0530

    code

--- CODEX_Project/src/App.jsx
+++ CODEX_Project/src/App.jsx
@@ -9,6 +9,7 @@ import Second from './Components/HOME/second/second.jsx'
 import AboutUs from './Components/HOME/firstpage/aboutUs.jsx'
 import Card from './Components/second page/Card.jsx'
 import SemSelect from './Components/second page/Academic/SemSelect.jsx'
+import NotesSection from './Components/second page/Academic/NotesSection.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,12 +18,17 @@ function App() {
     <>
       <Navbar />
       
-      <SemSelect />
+      {/* <SemSelect />
       <Card
         image="https://cdn.pixabay.com/photo/2025/07/16/15/01/karlsbad-9718003_1280.jpg"
         title="Building"
         description="A beautiful building in the heart of the city."
-      />
+      /> */}
+      {/* <Front /> */}
+      {/* <Logos /> */}
+      {/* <Second /> */}
+      {/* <AboutUs /> */}
+      <NotesSection />
     </>
   )
     // <>
